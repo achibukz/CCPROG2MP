9fafc100a49807e4eb9b7a4fb343e2a033cc5f89
Kaixyyyy <akibukuhan10@gmail.com>
2024-03-30 23:56:06 +0800

    Added Documentation

--- profunc.h
+++ profunc.h
@@ -33,26 +33,31 @@ struct Cell {
 void blank();
 void printProfile(Profile profile);
 void printBoards(Profile profile);
+void printBoardTex(struct Cell board[][15], int boardRows, int boardColumns,
+                int fog, FILE *file);
+
 void arrProf(string names[], int *numNames);
 void selSort(string arr[], int n);
+void sortProf(Profile Prof[], int numPlay);
+
+void cursorStart(FILE *file, string target);
 int profFinder(string arr[], string name, int n);
+void rewriteFile(FILE *sourceFile, FILE *destFile, long endPos);
+
 Profile createProfile();
 void selProfile();
 void viewStat(string name);
-void cursorStart(FILE *file, string target);
-void profileChanger(Profile *profile, int type, int diff, int win, int leader);
 void delProfile1(string name);
 void delProfile2(string name);
+void leaderBoards();
 void mainMenu(Profile *profile);
+
+void profileChanger(Profile *profile, int type, int diff, int win, int leader);
 void profile_mainMenu();
-void printBoardTex(struct Cell board[][15], int boardRows, int boardColumns,
-                int fog, FILE *file);
+
 void copy(string target);
 void paste();
-void rewriteFile(FILE *sourceFile, FILE *destFile, long endPos);
-void sortProf(Profile Prof[], int numPlay);
-void leaderBoards();
-void manipulate(string target, string cope, struct Cell board[][15], char state, int boardRows, int boardColumn, Profile *profile, int n);
+void manipulate(string target, string cope, struct Cell board[][15], char state, int boardRows, int boardColumn, Profile *profile);
 
 
 #endif
